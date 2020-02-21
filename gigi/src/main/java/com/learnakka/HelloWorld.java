319465ca507c5772620e7911e043f73b2f780387
tommuns <60960799+tommuns@users.noreply.github.com>
2020-02-21 14:42:13 +0100

    Remove duplication

--- gigi/src/main/java/com/learnakka/HelloWorld.java
+++ gigi/src/main/java/com/learnakka/HelloWorld.java
@@ -1,37 +1,7 @@
 package com.learnakka;
 
 public class HelloWorld {
-	
 	public  int add(int first, int second){
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
-		int third = first+second;
 		int third = first+second;
 		return third;
 	}
